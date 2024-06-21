20dff8f7c4a969f3bd9227f639b0406cca40386b
Ipieh <pierina1000a@gmail.com>
2024-06-21 16:37:15 +0200

    top ten names

--- viz1.js
+++ viz1.js
@@ -1,5 +1,6 @@
 let all_data = [];
 let data_per_year = Array.from({ length: 2021 }, () => []);
+
 let colorPerName = {};
 
 let svgContainer;
@@ -44,23 +45,118 @@ document.addEventListener('DOMContentLoaded', function () {
                 })
                 console.log("Colors created for names");
 
-                document.getElementById('title').textContent = "Explore the names in France"; 
+                document.getElementById('title').textContent = "France Name Race"; 
             })
 
-        // Slider management
-
-        // SLiders
-        let yearInput = document.getElementById('year');
-        // Values displayed below sliders
-        let yearDisplay = document.getElementById('yearValue');
-        // Year Interval 
-        let year = 1900;
+          // SLiders
+          
+          let yearInput = document.getElementById('year');
+          // Values displayed below sliders
       
+          let yearDisplay = document.getElementById('yearValue');
+          // Year Interval 
+          let year = 1900;
+  
+          document.getElementById('year').addEventListener('input', () => {
+              year = parseInt(yearInput.value);
+    
 
-        document.getElementById('year').addEventListener('input', () => {
-            year = parseInt(yearInput.value);
             yearDisplay.textContent = year.toString();
-            
-        });
+
+              minYearDisplay.textContent = minYear.toString();
+
+          });
+  
 
     })
+
+    // Function to add a label in the center of a svg path (Only used at loading page at the beginning)
+function createLabel(path,labelText){
+
+    const bbox = path.getBBox();
+    let centerX = bbox.x + bbox.width / 2;
+    let centerY = bbox.y + bbox.height / 2;
+    
+    // Creating the text element
+    const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+    text.setAttribute("x", centerX);
+    text.setAttribute("y", centerY);
+    text.setAttribute("text-anchor", "middle");
+    text.setAttribute("alignment-baseline", "central");
+    text.style.fontSize = "12px";
+    text.style.fill = "black"; // Set the text color
+    text.textContent = labelText; // Replace with your actual label
+
+    // Append the text element to the SVG container
+    svgContainer.querySelector("svg").appendChild(text);
+
+    return text;
+}
+
+// It show the most popular name for a fixed year (not used anymore but interesting)
+function showDataOneYear(year){ 
+
+    let maxPerDpt = Array(96).fill(0);
+    let namePerDpt = Array(96).fill("");
+
+    let max10ppl = Array(10).fill("");
+    let max10 = Array(10).fill(0);
+
+    data_per_year[year].forEach(element => {
+        const dpt = element[3];
+
+        if(element[4] > maxPerDpt[dpt]){
+            namePerDpt[dpt] = element[1];
+            maxPerDpt[dpt] = element[4]
+        }
+
+    })
+}
+
+function groupByName(){
+
+    // Compute the best name per year
+    for (let i = 1900; i <= 2020; i++) {
+        let best10 = Array(10).fill(0);
+        let best10names= Array(10).fill("");
+
+        let name_count= {};
+
+        data_per_year[i].forEach(element => {
+            const name = element[1];
+            const count = element[4];
+
+            if (!(name in name_count_masc)) {
+               name_count_masc[name] = 0;
+            }
+            name_count[name] += count;
+
+
+        });
+
+        for (const [name, count] of Object.entries(name_count_masc)) {
+            for(let i = 0; i<10; i++){
+            if (count > best10[i]) {
+                    shift_down(i,best10,best10names)
+                    best10[i]=count;
+                    best10names[i] = name;
+            }
+        }
+
+   
+        }
+        best_10names_per_year[i] = [best10,best10names];
+        }
+
+    }
+function shift_down(i, tableint, tablestring){
+    if (i==9){
+        return;
+    }
+    else{
+        shift_down(i+1,tablestring,tablestring);
+        tableint[i+1]=tableint[i];
+        tablestring[i+1]=tableint[i];
+
+    }
+}
